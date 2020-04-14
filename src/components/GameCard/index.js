60fd36443ce91b70e18752a3ff756a9f7ca38eaa
joelzehring <joelzehring@gmail.com>
2020-04-14 07:32:28 -0700

    ScoreBoard and image sources

--- src/components/GameCard/index.js
+++ src/components/GameCard/index.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import "./style.css";
 
 class GameCard extends Component {
-
+  constructor(props) {
+    super(props)
+    console.log(this.props.animalData.imgLink);
+  }
   render () {
     return (
-      <div>
-	<img src="#" alt="#" className="game-card" />
+      <div className='game-card'>
+	<img src={ this.props.animalData.imgLink } alt={ this.props.animalData.name } className="img-game-card" />
       </div>
     )
   }
-
 }
 
 export default GameCard;
---
+++ src/components/ScoreBoard/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css';
+
+function ScoreBoard () {
+
+
+  return (
+
+    <div className="score-board">
+      <p>Score: <span></span></p>
+      <p>High Score: <span></span></p>
+    </div>
+
+  );
+}
+
+export default ScoreBoard;
